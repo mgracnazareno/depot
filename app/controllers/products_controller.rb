4004d60dda77140cbb12b48aabee9be885d8d3d5
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-07-09 02:50:06 -0400

    Implement Product controller actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,38 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(params[:product])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :image, :price)
+  end
 end
